e786264cbb9cf160c7003c6a39eebb93cfe7cb76
ovaar <1405257+ovaar@users.noreply.github.com>
2025-11-05 22:22:40 +0100

    build(ccache.cmake): Added setting CMAKE_MSVC_DEBUG_INFORMATION_FORMAT as "Embedded" (/Z7) for ccache compatibility

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -10,4 +10,11 @@ if(USE_CCACHE)
   message(STATUS "Using ccache for compilation.")
   set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
   set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
+  if (MSVC)
+    # ccache is incompatible with `/Zi` or `/ZI` and needs `/Z7`, see
+    # - https://discourse.cmake.org/t/early-experiences-with-msvc-debug-information-format-and-cmp0141/6859
+    # - https://learn.microsoft.com/en-us/cpp/build/reference/z7-zi-zi-debug-information-format?view=msvc-170
+    # - https://cmake.org/cmake/help/latest/variable/CMAKE_MSVC_DEBUG_INFORMATION_FORMAT.html
+    set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "Embedded")
+  endif()
 endif()
